3cc4bac6077b21a706aaaa7b7e8db095d13f2c3d
Zach Nichols <znichols1131@gmail.com>
2021-11-09 20:15:22 -0500

    Ticket 001b - Setup rating class

---
+++ AgileProject/Models/Rating.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AgileProject.Models
+{
+    public class Rating
+    {
+        [Key]
+        public int RatingId { get; set; }
+
+        [Required, Range(0.0d, 5.0d, ErrorMessage = "Error: score must be between 0.0 and 5.0")]
+        public double Score { get; set; }
+
+        [MaxLength(240, ErrorMessage = "Error: reviews must be 240 characters or less.")]
+        public string Review { get; set; }
+
+        [ForeignKey("User")]
+        public Guid UserId { get; set; }
+    }
+}
